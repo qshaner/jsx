70bc9a19219694f1cb38ed8fa4af167e6d833ba6
Quinn Shaner <qshaner@launchdarkly.com>
2022-01-31 13:25:23 -0500

    1-5 module, not componentized

--- src/index.js
+++ src/index.js
@@ -1,17 +1,66 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom"
+import faker from "faker"
+
+const App = function() {
+
+return (
+  <div className="ui container comments">
+    <div className="comment">
+      <a href="/" className="avatar">
+        <img alt="avatar" src={faker.image.image()} ></img>
+      </a>
+      <div className="content">
+        <a href="/" className="author">
+          Sam Jones
+        </a>
+        <div className="metadata">
+          <span className="date">Today at 6:00pm</span>  </div>
+      </div>
+      <div className="text">
+        Nice!
+      </div>
+    </div>
+    <div className="comment">
+      <a href="/" className="avatar">
+        <img alt="avatar" src={faker.image.image()} ></img>
+      </a>
+      <div className="content">
+        <a href="/" className="author">
+ Jones Antha</a>
+        <div className="metadata">
+          <span className="date">Today at 4:00pm</span>  </div>
+      </div>
+      <div className="text">
+        Nice!
+      </div>
+    </div>
+    <div className="comment">
+      <a href="/" className="avatar">
+        <img alt="avatar" src={faker.image.image()} ></img>
+      </a>
+      <div className="content">
+        <a href="/" className="author">
+Tim        </a>
+        <div className="metadata">
+          <span className="date">Today at 8:00pm</span>  </div>
+      </div>
+      <div className="text">
+        Eyyyy
+      </div>
+    </div>
+  </div>
+)
+}
+
+//equivalent to 
+/* 
+const App = () => {
+  return <div>Hi there!</div>
+}
+*/
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  <App />,
+  document.querySelector('#root')
+)
